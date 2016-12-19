74065985d6b2933f1c67ce8cbb0d86d894f98465
Alexander Mishyn <mishyn@gmail.com>
2016-12-19 19:53:07 +0100

    add view controls

--- OneLineToggl/ViewController.swift
+++ OneLineToggl/ViewController.swift
@@ -7,21 +7,88 @@
 //
 
 import Cocoa
+//import Alamofire
+//import SwiftDate
 
 class ViewController: NSViewController {
-
+    let slang = ["#ks": ***REMOVED***, "#p2b": ***REMOVED***]
+    
+    @IBOutlet weak var slangLabel: NSTextField!
+    @IBOutlet weak var lastActivity: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        self.slangLabel.stringValue = slang.description
     }
-
+    
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-
+    
+    @IBAction func textFieldAction(_ sender: NSTextField) {
+        let string = sender.stringValue
+        var title = string
+        print(string);
+        
+        var project: String = ""
+        let pattern = "\\#\\w+"
+        
+        do {
+            let regex = try NSRegularExpression(pattern: pattern)
+            let nsString = string as NSString
+            let results = regex.matches(in: string, range: NSRange(location: 0, length: nsString.length))
+            results.map {
+                project = nsString.substring(with: $0.range)
+                title = title.replacingOccurrences(of: project, with: "")
+            }
+            
+            
+        } catch let error {
+            print("invalid regex: \(error.localizedDescription)")
+        }
+        
+        let togglProject = slang[project]
+        if (togglProject != nil) {
+            print("activity:", title, " @ ", project, togglProject!)
+            
+            //            var headers: HTTPHeaders = [
+            //                "Content-Type": "application/json"
+            //            ]
+            //
+            //            let date = Date()
+            //            let dateInRegion = DateInRegion(absoluteDate: date)
+            //            let serializedString:String = dateInRegion.iso8601()
+            //            let parameters = [
+            //                "time_entry": ["start": serializedString,
+            //                               "description": title,
+            //                               "pid": togglProject!,
+            //                               "created_with": "One line",
+            //                               "duration": -1*date.timeIntervalSince1970
+            //                    ] as [String : Any]
+            //            ]
+            //
+            //            let user = "***REMOVED***"
+            //            let password = "api_token"
+            //
+            //            if let authorizationHeader = Request.authorizationHeader(user: user, password: password) {
+            //                headers[authorizationHeader.key] = authorizationHeader.value
+            //            }
+            
+            
+            //            Alamofire.request("https://www.toggl.com/api/v8/time_entries", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).response { response in
+            //                sender.stringValue = ""
+            //                self.lastActivity.stringValue = "\(title) \(project)"
+            //            }
+            
+        }
+        
+    }
+    
 }
+
 
