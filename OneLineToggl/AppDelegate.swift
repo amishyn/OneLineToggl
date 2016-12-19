71a38c24a99ab111130d72e9f6534fda6f265dc5
Alexander Mishyn <mishyn@gmail.com>
2016-12-19 19:48:06 +0100

    Initial Commit

---
+++ OneLineToggl/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OneLineToggl
+//
+//  Created by Alex Mishyn on 19/12/2016.
+//  Copyright © 2016 Alex Mishyn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OneLineToggl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OneLineToggl
+//
+//  Created by Alex Mishyn on 19/12/2016.
+//  Copyright © 2016 Alex Mishyn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
